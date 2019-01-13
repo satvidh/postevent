1c4060d1eaeb5fc86a2b525e7c8539ab21d446ff
Satish Kumar <satish.kumar@kasasa.com>
2019-01-12 18:41:42 -0600

    Implement associations route. Still does not work completely.

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -7,11 +7,12 @@ class AssociationsController < ApplicationController
 		nonce = params[:nonce]
 		association = Association.find_by_nonce(nonce)
 		if association
-			current_user.update_attributes({:slack_user_id => association.user_id})
+			@user = current_user
+			@user.update_attributes({:slack_user_id => association.user_id})
 			client = Slack::Web::Client.new(token: ENV['SLACK_API_TOKEN'])
 			client.auth_test
 			client.chat_postMessage(channel: association.user_id,
-									text: "You (#{user_name}) are now authorized to post events from slack.",
+									text: "You are now authorized to post events from slack.",
 									as_user: true)
 		else
 			client.chat_postMessage(channel: "PostEvent", text: "Could not authorize user.", as_user: false)

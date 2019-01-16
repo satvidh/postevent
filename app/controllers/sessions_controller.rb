edfb3c947fceab8be6261133925d5fe0a43bf5fe
Satish Kumar <kumarnotifications@gmail.com>
2019-01-16 01:29:19 -0600

    Intermediate check-in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+import 'securerandom'
+
 class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
@@ -14,4 +16,16 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url, :notice => "Logged out!"
   end
+
+  def slack
+    association_nonce = nonce
+    association = Associations.
+    flash.now.alert = "Login to associate slack user"
+    render "new"
+  end
+
+private:
+    def nonce
+        SecureRandom.hex()
+    end
 end
---
+++ app/models/associations.rb
@@ -0,0 +1,11 @@
+class Associations < ActiveRecord::Base
+  attr_accessible :user_id, :nonce, :timestamp
+  has_secure_password
+  validates :email, :presence => true, :uniqueness => true, :length => {:maximum => 255}
+  validates :password, :length => {:maximum => 255}
+  validates_presence_of :password, :on => :create
+
+  def authorized_for_linkedin
+    linkedin_token_expiration != nil && linkedin_token_expiration > DateTime.now
+  end
+end

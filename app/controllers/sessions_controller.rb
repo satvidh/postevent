d21d398565b4fa09bdaf1c37f7f15a3ceb981cd7
Satish Kumar <kumarnotifications@gmail.com>
2019-01-17 02:01:42 -0600

    Cleanup.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,19 @@
 require 'securerandom'
-require 'slack-ruby-client'
 require 'logger'
 
 class SessionsController < ApplicationController
   def new
-    logger.debug("Remove nonce from session")
-    session[:nonce] = nil
+    remove_nonce
     render "new"
   end
   def create
-    logger.debug("Remove nonce from session")
-    session[:nonce] = nil
+    remove_nonce
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       association_nonce = params[:nonce]
       if not association_nonce.nil? and not association_nonce.empty?
           association = Associations.find_by_nonce(association_nonce)
           time_now = Time.now
-          logger.info("expiration time #{association.nonce_expiration_time} and now #{time_now}")
           if not association or time_now >= association.nonce_expiration_time
             render "slack_expired_nonce" and return
           else
@@ -32,11 +28,12 @@ class SessionsController < ApplicationController
       session[:nonce] = params[:nonce]
       flash.now.alert = "Invalid email or password"
       render "new"
+      remove_nonce
     end
   end
 
   def destroy
-    session[:user_id] = nil
+    logout_user
     redirect_to root_url, :notice => "Logged out!"
   end
 
@@ -46,19 +43,36 @@ class SessionsController < ApplicationController
     if association and Time.now < association.nonce_expiration_time
         user = User.find_by_slack_user_id(association.user_id)
         if not user
+          # Expire the current association so the link stays one time use only.
+          association.update_attributes(:nonce_expiration_time => Time.now)
           flash.now.alert = "Log in to link slack user to PostEvent"
-          session[:nonce] = association_nonce
+          logout_user
+          new_association_nonce = SecureRandom.hex()
+          new_association = Associations.new(:user_id => association.user_id,
+                                             :nonce => new_association_nonce,
+                                             :nonce_expiration_time => 30.seconds.from_now)
+          new_association.save
+          session[:nonce] = new_association_nonce
           render "new"
-          logger.debug("Remove nonce from session")
-          session[:nonce] = nil
+          remove_nonce
         else
           association.update_attributes(:nonce_expiration_time => Time.now)
-          session[:user_id] = user.id
           logger.debug("Redirect to root with user_id #{session[:user_id]}")
           redirect_to :controller => "events", :action => "new"
         end
     else
+        logout_user
         render "slack_expired_nonce"
     end
   end
+
+private
+   def remove_nonce
+    logger.debug("Remove nonce from session")
+    session[:nonce] = nil
+   end
+
+   def logout_user
+      session[:user_id] = nil
+   end
 end
